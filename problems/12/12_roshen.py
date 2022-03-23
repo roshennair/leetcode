da581e35a60d873c7d7f5d12ac1b87f3bfda5b27
Roshen Nair <roshen620@gmail.com>
2022-03-23 08:32:04 +0800

    Optimised my Q12 solution for speed

--- problems/12/12_roshen.py
+++ problems/12/12_roshen.py
@@ -6,22 +6,22 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         value_to_symbol = {
-            1: 'I',
-            4: 'IV',
-            5: 'V',
-            9: 'IX',
-            10: 'X',
-            40: 'XL',
-            50: 'L',
-            90: 'XC',
-            100: 'C',
-            400: 'CD',
-            500: 'D',
+            1000: 'M',
             900: 'CM',
-            1000: 'M'
+            500: 'D',
+            400: 'CD',
+            100: 'C',
+            90: 'XC',
+            50: 'L',
+            40: 'XL',
+            10: 'X',
+            9: 'IX',
+            5: 'V',
+            4: 'IV',
+            1: 'I',
         }
         numeral = ''
-        for val in reversed(sorted(value_to_symbol.keys())):
+        for val in value_to_symbol.keys():
             if num // val > 0:
                 numeral += value_to_symbol[val] * (num // val)
                 num %= val
@@ -29,5 +29,5 @@ class Solution:
 
 
 sol = Solution()
-input = 1994
+input = 3
 print('Answer:', sol.intToRoman(input))
